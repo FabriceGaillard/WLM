b176e6a7b133698bfe5dba65ce6897b15ae7fb56
Fab <fabou291@gmail.com>
2022-05-12 22:45:17 +0200

    improve user migration #10

--- back/database/migrations/1652206809539_users.ts
+++ back/database/migrations/1652206809539_users.ts
@@ -1,5 +1,5 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
-
+import { gender, status } from 'App/Models/User'
 export default class UsersSchema extends BaseSchema {
     protected tableName = 'users'
 
@@ -10,9 +10,19 @@ export default class UsersSchema extends BaseSchema {
             table.string('password', 180).notNullable()
             table.string('remember_me_token').nullable()
 
-            table.string('username').nullable()
-            table.enum('status', ['online', 'busy', 'beRightBack', 'away', 'onThePhone', 'outToLunch', 'appearOffline']).defaultTo('online')
+            table.string('username', 255).nullable()
+            table.string('personalMessage', 255).nullable()
+            table.enum('status', Object.values(status)).defaultTo(status.ONLINE)
             table.string('avatar', 255).notNullable()
+
+            table.string('first_name', 255).notNullable()
+            table.string('last_name', 255).notNullable()
+            table.enum('gender', Object.values(gender)).notNullable()
+            table.integer('birth_year', 255).nullable()
+            table.string('alternate_email', 255).notNullable()
+            table.string('country', 255).notNullable().defaultTo('france')
+            table.string('state', 255).nullable()
+            table.string('zip_code', 5).nullable()
 
             table.timestamp('created_at').notNullable()
             table.timestamp('updated_at').notNullable()
