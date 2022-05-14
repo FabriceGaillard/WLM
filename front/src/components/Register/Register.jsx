918b03c50ea816dfbba826ef4a9ebc91e1eefa7f
Vincent Manzanera <vincent.mzapro@outlook.fr>
2022-05-14 16:26:26 +0200

    created register folder in styles/components path. Then created register.scss. Created french_departments file in data folder. That files contains a function return an object with all french departments and another function that calls french government API to return city name in relation zip code argument.

---
+++ front/src/components/Register/Register.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Register = () => {
+    return (
+        <div className='register'>
+            <form className='register__form'>
+                
+            </form>
+        </div>
+    );
+};
+
+export default Register;
