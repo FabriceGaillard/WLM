cb72bce0e789649428c2634c8b86d6ac7be91754
David Michel <david.michel2809@gmail.com>
2022-05-19 13:16:22 +0200

    feat: adding autologin base logic and link it to the login context. style: making checkbox style

--- front/src/components/Login/LoginAuto.jsx
+++ front/src/components/Login/LoginAuto.jsx
@@ -1,8 +1,27 @@
+// HOOKS
+import { useContext, useRef } from 'react';
+// CONTEXT
+import loginContext from '../../contexts/LoginContext';
+
 const LoginAuto = () => {
+
+  const { formUpdate, setFormUpdate } = useContext(loginContext);
+  const autoAuthCheckRef = useRef();
+
+  const handleRememberLogin = () => {
+    const { checked } = autoAuthCheckRef.current;
+    setFormUpdate(prev => ({ ...prev, autoAuth: checked }));
+  };
 
   return (
     <div className="auto-login__container">
-      <input type="checkbox" id="auto-login" />
+      <input
+        type="checkbox"
+        id="auto-login"
+        ref={autoAuthCheckRef}
+        checked={formUpdate.autoAuth}
+        onChange={handleRememberLogin}
+      />
       <label htmlFor="auto-login">Connexion automatique</label>
     </div>
   );
