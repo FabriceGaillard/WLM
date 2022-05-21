fc4a3d3046d831a60a3a76bf0f4d47493e786783
Vincent Manzanera <vincent.mzapro@outlook.fr>
2022-05-21 15:50:12 +0200

    RegisterCaptcha : started to build code in order to create captcha logic before user send the filled form to the backend. So I'm using faker to randomly provide alphanumeric data when user clicks on gereate new captcha button

--- front/src/components/Register/RegisterCaptcha.jsx
+++ front/src/components/Register/RegisterCaptcha.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
-
+import React, { useEffect, useState, useRef } from "react";
+import { faker } from 'https://cdn.skypack.dev/@faker-js/faker';
 const RegisterCaptcha = () => {
+    let value = ""
+    //CHECK IF USER GENERATE A NEW CAPTCHA
+    const[isChanged, setIsChanged]=useState(false)
+    //FOCUS ON INPUT VALUE
+    const inputElement = useRef()
+    //LISTEN TO IS CHANGED STATE
+    useEffect(()=>{
+        value = faker.random.alphaNumeric(5)
+        inputElement.current.value = value
+        setIsChanged(false)
+    },[isChanged])
+    //LISTEN CLICK ON GENERATE NEW CAPTCHA BUTTON
+    const changeCaptcha = ()=>{
+        setIsChanged(true)
+    }
     return (
-        <div>
-            <h1>Captcha ici</h1>
+        <div className='register-captcha'>
+            <div className="register-container">
+                <input 
+                type="text" 
+                style={{backgroundColor: "grey", textDecorationLine: "line-through"}}
+                ref={inputElement}
+                />    
+                <input type="button" value="Générer captcha" onClick={changeCaptcha}/>
+            </div>
         </div>
     );
 };
