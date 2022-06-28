a373680c3332df3cb9e73c228637f831adaf56e7
David Michel <david.michel2809@gmail.com>
2022-06-28 13:24:20 +0200

    init: adding Home route and component #42

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,13 +2,15 @@ import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, } from "react-router-dom";
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register/>}/>
+        <Route path='/register' element={<Register />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ front/src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>Connected 😎</div>
+  );
+};
+
+export default Home;
