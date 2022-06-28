5c82848f0e9846a590319a9760cadbe5d56313c2
David Michel <david.michel2809@gmail.com>
2022-06-28 10:05:49 +0200

    feat: adding loginRequest function, and modifying handleLoginSubmit #42

--- front/src/components/Login/LoginSubmit.jsx
+++ front/src/components/Login/LoginSubmit.jsx
@@ -1,17 +1,50 @@
 // HOOKS
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 // CONTEXT
 import loginContext from '../../contexts/LoginContext';
 
 const LoginSubmit = () => {
 
-  const { isConnecting, setIsConnecting } = useContext(loginContext);
+  const loginPath = "http://localhost:3333/api/auth/login";
+  const { isConnecting, setIsConnecting, formUpdate } = useContext(loginContext);
+
+  const [isConnected, setIsConnected] = useState(false);
 
   const handleLoginSubmit = () => {
-    isConnecting
-      ? setIsConnecting(false)
-      : setIsConnecting(true);
+    if (isConnecting === false) {
+      loginRequest();
+      setIsConnecting(true);
+    }
+    else {
+      setIsConnecting(false);
+    }
   };
+
+  const loginRequest = async () => {
+    const { email, password, autoAuth: remember } = formUpdate;
+    const options = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password, remember }),
+      credentials: "include"
+    };
+
+    try {
+      const response = await fetch(loginPath, options);
+      if (response.ok) {
+        const data = await response.json();
+        setIsConnected(true);
+        console.log(data);
+      }
+    }
+    catch (err) {
+      console.log(err);
+    }
+    finally {
+      setIsConnecting(false);
+    }
+  };
+
 
   return (
     <input
