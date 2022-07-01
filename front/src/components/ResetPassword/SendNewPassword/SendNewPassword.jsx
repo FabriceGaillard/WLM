8d274de06b5bc3b6bff6a758bd33e9a0f55e87bd
David Michel <david.michel2809@gmail.com>
2022-07-01 09:31:39 +0200

    feat: adding decoding signature logic from url token param 45

--- front/src/components/ResetPassword/SendNewPassword/SendNewPassword.jsx
+++ front/src/components/ResetPassword/SendNewPassword/SendNewPassword.jsx
@@ -3,7 +3,26 @@ import { useSearchParams, useNavigate } from 'react-router-dom';
 
 const SendNewPassword = () => {
 
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
 
+  const [urlTokenSignature, setUrlTokenSignature] = useState();
+
+  const getSignatureFromUrl = (url) => url.split("signature=")[1];
+
+  useEffect(() => {
+    const getUrlToken = searchParams.get("token");
+    if (!getUrlToken) {
+      navigate("/login");
+    }
+
+    const decodedUrlToken = atob(getUrlToken);
+    setUrlTokenSignature(getSignatureFromUrl(decodedUrlToken));
+  }, []);
+
+  return (
+    <div>decoded: {urlTokenSignature}</div>
+  );
 };
 
 export default SendNewPassword;
