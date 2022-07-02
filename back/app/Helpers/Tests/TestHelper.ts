93458b8e7ad3f2aae563fca4236781cde0822434
Fab <fabou291@gmail.com>
2022-07-02 20:39:50 +0200

    refactor: add body parameter into methods of testHelper

--- back/app/Helpers/Tests/TestHelper.ts
+++ back/app/Helpers/Tests/TestHelper.ts
@@ -6,25 +6,25 @@ import RulesHelper from "./RulesHelper";
 
 export default class TestHelper {
 
-    public static ressourceNotFound(ressource: string, method: string, endpoint: string) {
-        test(`it should FAIL (404) when ${ressource} is not found`, async ({ client }) => {
+    public static ressourceNotFound(ressource: string, method: string, endpoint: string, body = {}) {
+        test(`should FAIL (404) when ${ressource} is not found`, async ({ client }) => {
             const user = await User.firstOrFail()
-            const response = await client[method](endpoint).loginAs(user)
+            const response = await client[method](endpoint).loginAs(user).json(body)
             ResponseAssertHelper.error404(response, 'E_ROW_NOT_FOUND: Row not found')
         })
     }
 
     public static notAuthenticated(method: string, endpoint: string) {
-        test('it should FAIL (401) when user is not authenticated', async ({ client }) => {
+        test('should FAIL (401) when user is not authenticated', async ({ client }) => {
             const response = await client[method](endpoint)
             ResponseAssertHelper.error401(response)
         })
     }
 
-    public static ressourceIdInvalid(ressource: string, method: string, endpoint: string) {
-        test(`it should FAIL (422) when ${ressource} id is an invalid uuid`, async ({ client }) => {
+    public static ressourceIdInvalid(ressource: string, method: string, endpoint: string, body = {}) {
+        test(`should FAIL (422) when ${ressource} id is an invalid uuid`, async ({ client }) => {
             const user = await User.firstOrFail()
-            const response = await client[method](endpoint).loginAs(user)
+            const response = await client[method](endpoint).loginAs(user).json(body)
             ResponseAssertHelper.error422(response, [RulesHelper.uuid('params.id')])
         })
     }
