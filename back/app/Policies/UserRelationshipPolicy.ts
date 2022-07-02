98ca78d1dded9705f053ae0304f62b0b767d275c
Fab <fabou291@gmail.com>
2022-07-02 20:41:50 +0200

    refactor: update/add policies for userRelationship

--- back/app/Policies/UserRelationshipPolicy.ts
+++ back/app/Policies/UserRelationshipPolicy.ts
@@ -3,15 +3,13 @@ import User from 'App/Models/User'
 import UserRelationship from 'App/Models/UserRelationship'
 
 export default class UserRelationshipPolicy extends BasePolicy {
-    public async viewList(user: User, userId: string) {
-        return userId === user.id
-    }
     public async view(user: User, UserRelationship: UserRelationship) {
         return UserRelationship.relatingUserId === user.id
     }
-    public async create(user: User, userId: string) {
-        return userId === user.id
+    public async update(user: User, UserRelationship: UserRelationship) {
+        return UserRelationship.relatingUserId === user.id
     }
+
     public async delete(user: User, UserRelationship: UserRelationship) {
         return UserRelationship.relatingUserId === user.id
     }
