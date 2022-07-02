f32c86d1acd4158e3554ddb39c35e689728b97d1
Fab <fabou291@gmail.com>
2022-07-02 18:33:08 +0200

    refactor: update routes of userRelationships

--- back/start/routes/userRelationshipRoutes.ts
+++ back/start/routes/userRelationshipRoutes.ts
@@ -1,5 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-    Route.resource('user-relationships', 'UserRelationshipsController').apiOnly().except(['update'])
-}).prefix('api/users/:userId').middleware(['auth'])
+    Route.get('user-relationships/index-of-authenticated-user', 'UserRelationshipsController.indexOfAuthenticatedUser')
+    Route.post('user-relationships/store-or-makes-visible', 'UserRelationshipsController.storeOrMakesVisible')
+
+    Route.resource('user-relationships', 'UserRelationshipsController').apiOnly().except(['store', 'index', 'destroy'])
+}).prefix('api').middleware(['auth'])
