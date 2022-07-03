020acf56831c83a86fce204e18ce3dda3c393481
Fab <fabou291@gmail.com>
2022-07-03 13:43:03 +0200

    feat: implement block and unblock methods

--- back/app/Controllers/Http/UserRelationshipsController.ts
+++ back/app/Controllers/Http/UserRelationshipsController.ts
@@ -4,6 +4,10 @@ import User from 'App/Models/User';
 import ShowUserRelationshipValidator from 'App/Validators/UserRelationship/ShowUserRelationshipValidator';
 import StoreUserRelationshipValidator from 'App/Validators/UserRelationship/StoreUserRelationshipValidator';
 import UpdateUserRelationshipValidator from 'App/Validators/UserRelationship/UpdateUserRelationshipValidator';
+import BlockedUserRelationshipLog from 'App/Models/BlockedUserRelationshipLog';
+import { DateTime } from 'luxon';
+import BlockUserRelationshipValidator from 'App/Validators/UserRelationship/BlockUserRelationshipValidator';
+import UnblockUserRelationshipValidator from 'App/Validators/UserRelationship/UnblockUserRelationshipValidator copy';
 
 export default class UserRelationshipsController {
     public async indexOfAuthenticatedUser({ auth, response }: HttpContextContract) {
@@ -56,6 +60,49 @@ export default class UserRelationshipsController {
         await userRelationship.merge(payload).save()
         await userRelationship.load('relatedUser')
         return response.ok(userRelationship)
+    }
+
+    public async block({ request, response, bouncer }: HttpContextContract) {
+        const payload = await request.validate(BlockUserRelationshipValidator)
+        const userRelationship = await UserRelationship.findOrFail(payload.params.id)
+
+        await bouncer
+            .with('UserRelationshipPolicy')
+            .authorize('update', userRelationship)
+
+        if (userRelationship.isBlocked) return response.badRequest()
+
+        await userRelationship.merge({ isBlocked: true }).save()
+
+        await BlockedUserRelationshipLog.create({
+            relatedUserId: userRelationship.relatedUserId,
+            relatingUserId: userRelationship.relatingUserId,
+        })
+
+        return response.noContent()
+    }
+
+    public async unblock({ request, response, bouncer }: HttpContextContract) {
+        const payload = await request.validate(UnblockUserRelationshipValidator)
+        const userRelationship = await UserRelationship.findOrFail(payload.params.id)
+
+        await bouncer
+            .with('UserRelationshipPolicy')
+            .authorize('update', userRelationship)
+
+        if (!userRelationship.isBlocked) return response.badRequest()
+
+        await userRelationship.merge({ isBlocked: false }).save()
+
+        const lastBlockedUserRelationshipLog = await BlockedUserRelationshipLog.query()
+            .where('related_user_id', userRelationship.relatedUserId)
+            .andWhere('relating_user_id', userRelationship.relatingUserId)
+            .orderBy('started_at', 'desc')
+            .firstOrFail()
+
+        lastBlockedUserRelationshipLog.merge({ endedAt: DateTime.now() })
+
+        return response.noContent()
     }
 
 }
--- back/start/routes/userRelationshipRoutes.ts
+++ back/start/routes/userRelationshipRoutes.ts
@@ -3,6 +3,8 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
     Route.get('user-relationships/index-of-authenticated-user', 'UserRelationshipsController.indexOfAuthenticatedUser')
     Route.post('user-relationships/store-or-makes-visible', 'UserRelationshipsController.storeOrMakesVisible')
+    Route.patch('user-relationships/:id/block', 'UserRelationshipsController.block')
+    Route.patch('user-relationships/:id/unblock', 'UserRelationshipsController.unblock')
 
     Route.resource('user-relationships', 'UserRelationshipsController').apiOnly().except(['store', 'index', 'destroy'])
 }).prefix('api').middleware(['auth'])
