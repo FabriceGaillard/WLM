9edfde30a93d3118ebb697e2cd8be3f1fafcbc4a
David Michel <david.michel2809@gmail.com>
2022-07-05 15:53:47 +0200

    feat: reworking LoginSubmit logic #48

--- front/src/components/Login/LoginSubmit.jsx
+++ front/src/components/Login/LoginSubmit.jsx
@@ -5,51 +5,63 @@ import { useNavigate } from 'react-router-dom';
 import loginContext from '../../contexts/LoginContext';
 import globalContext from '../../contexts/GlobalContext';
 // HELPERS
-import { fetchLogin } from '../../helpers/fetch';
+import { fetchLogin, fetchMeFromLogin } from '../../helpers/fetch';
 import handleStorageWhenAuthenticated from '../../helpers/handleStorageWhenAuthenticated';
 
 const LoginSubmit = () => {
-  const { isConnecting, setIsConnecting, formUpdate } = useContext(loginContext);
+  const { isConnecting, setIsConnecting, formUpdate, storageData } = useContext(loginContext);
   const { userDataFromDb, setUserDataFromDb } = useContext(globalContext);
   const abortController = useRef(null);
   const navigate = useNavigate();
 
   const [isAborting, setIsAborting] = useState(false);
+  const [hasClickedSubmitBtn, setHasClickedSubmitBtn] = useState(false);
 
-  const loginRequest = async () => {
+  const meOrLoginRequest = async (requestChoice) => {
     const { email, password, autoAuth: remember } = formUpdate;
-
+    let currentUser;
     abortController.current = new AbortController();
-    const currentUser = await fetchLogin({ email, password, remember }, abortController.current);
+
+    if (requestChoice === "login") {
+      currentUser = await fetchLogin({ email, password, remember }, abortController.current);
+    }
+
+    if (requestChoice === "me") {
+      currentUser = await fetchMeFromLogin(abortController.current);
+    }
 
     setUserDataFromDb(currentUser);
     handleStorageWhenAuthenticated(currentUser, remember);
   };
 
   useEffect(() => {
-    const handleLoginSubmit = async () => {
+    if (isConnecting) {
+      const handleLoginSubmit = async () => {
+        try {
+          if (storageData?.remember === true && hasClickedSubmitBtn === false) {
+            await meOrLoginRequest("me");
+            return;
+          }
 
-      try {
-        if (isConnecting === true) {
-          await loginRequest();
+          await meOrLoginRequest("login");
         }
-      }
-      catch (err) {
-        console.log(err);
-      }
-      finally {
-        setIsConnecting(false);
-      }
-    };
+        catch (err) {
+          console.log(err);
+        }
+        finally {
+          setIsConnecting(false);
+        }
+      };
 
-    handleLoginSubmit();
+      handleLoginSubmit();
+    }
   }, [isConnecting]);
+
 
   useEffect(() => {
     if (isAborting === true && abortController.current) {
       abortController.current.abort();
       setIsAborting(false);
-      console.log("abort...");
     }
   }, [isAborting]);
 
@@ -66,6 +78,7 @@ const LoginSubmit = () => {
       id="connect__btn"
       onClick={() => {
         setIsConnecting(prev => !prev);
+        setHasClickedSubmitBtn(true);
         isConnecting && setIsAborting(true);
       }}
     />
