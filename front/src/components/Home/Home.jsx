5959696a4789aec7c24274b27575c53e11a18820
David Michel <david.michel2809@gmail.com>
2022-07-15 14:41:11 +0200

    feat: importing HeaderWithLogo on Home #63

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 // COMPONENTS
 import Contacts from "./Contacts/Contacts";
-import User from './User/User';
+import User from "./User/User";
+
+import HeaderWithLogo from "../HeaderWithLogo";
 
 const Home = () => {
   return (
     <div className="home__container">
       <div className="home">
+        <HeaderWithLogo />
         <User />
         <Contacts />
       </div>
