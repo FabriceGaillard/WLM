a7827fd66998ee4a5f2d417ccaec0d02d02bc408
David Michel <david.michel2809@gmail.com>
2022-07-21 09:56:04 +0200

    feat: fixing and simplifying UserPersonalMessage #65

--- front/src/components/Home/User/UserPersonnalMessage/UserPersonalMessage.jsx
+++ front/src/components/Home/User/UserPersonnalMessage/UserPersonalMessage.jsx
@@ -1,5 +1,5 @@
 // HOOKS
-import { useState, useContext, useRef, useEffect, useCallback } from 'react';
+import { useState, useContext, useRef, useEffect } from 'react';
 // CONTEXT
 import globalContext from '../../../../contexts/GlobalContext';
 
@@ -7,24 +7,14 @@ const UserPersonalMessage = () => {
 
   const { userDataFromDb, setUserDataFromDb } = useContext(globalContext);
   const inputRef = useRef();
-  const buttonRef = useRef();
 
   const [clickedButton, setClickedButton] = useState(false);
   const [personalMessage, setPersonalMessage] = useState(userDataFromDb.personalMessage || "<Tapez votre message perso>");
-  const [clickedTarget, setClickedTarget] = useState(null);
-  const [clickedButtonTarget, setClickedButtonTarget] = useState(null);
-  const handleListener = useCallback(({ target }) => {
-    console.log("🚀 ~ file: UserPersonalMessage.jsx ~ line 20 ~ handleListener");
-    setClickedTarget(target);
-  }, [clickedTarget]);
 
-
-
-  const handlePersonalMessage = ({ key }) => {
-    if (key === "Enter") {
-      setUserDataFromDb({ ...userDataFromDb, personalMessage: personalMessage });
-      setClickedButton(false);
-    }
+  const handlePersonalMessage = (event) => {
+    event.preventDefault();
+    setUserDataFromDb({ ...userDataFromDb, personalMessage: personalMessage });
+    setClickedButton(false);
   };
 
   const autoSelect = () => {
@@ -35,51 +25,23 @@ const UserPersonalMessage = () => {
 
   useEffect(autoSelect, [clickedButton]);
 
-
-  useEffect(() => {
-    if (inputRef.current) {
-      const isClickingOnButton = clickedButtonTarget.contains(clickedTarget);
-      const isClickingOnInput = inputRef.current.contains(clickedTarget);
-
-      if (!isClickingOnButton && !isClickingOnInput) {
-        setUserDataFromDb({ ...userDataFromDb, personalMessage: personalMessage });
-        setClickedButton(false);
-        document.removeEventListener("mousedown", handleListener);
-      }
-    }
-  }, [clickedTarget]);
-
-  useEffect(() => {
-    if (clickedButton) {
-      document.addEventListener("mousedown", handleListener);
-    }
-
-    return () => {
-      document.removeEventListener("mousedown", handleListener);
-    };
-  }, [clickedButton]);
-
   return (
-    <>
+    <form onSubmit={handlePersonalMessage}>
       {clickedButton
         ? (<input type="text"
           ref={inputRef}
           onChange={({ target }) => setPersonalMessage(target.value)}
-          onKeyDown={handlePersonalMessage}
+          onBlur={handlePersonalMessage}
           value={personalMessage}
         />)
         : <button
           type="button"
           className="user-personnal-message"
-          ref={buttonRef}
-          onClick={({ target }) => {
-            setClickedButton(true);
-            setClickedButtonTarget(target);
-          }}
+          onClick={() => setClickedButton(true)}
         >
           {personalMessage}
         </button>}
-    </>
+    </form>
   );
 };
 
