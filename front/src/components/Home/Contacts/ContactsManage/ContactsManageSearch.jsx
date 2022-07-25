a05ca1b448375a77b3098260f672f92f1c263372
David Michel <david.michel2809@gmail.com>
2022-07-25 11:53:50 +0200

    feat: adding reset button logic and style on input #67

--- front/src/components/Home/Contacts/ContactsManage/ContactsManageSearch.jsx
+++ front/src/components/Home/Contacts/ContactsManage/ContactsManageSearch.jsx
@@ -1,10 +1,20 @@
 // COMPONENTS
-import React from 'react';
+import { useState, useRef } from 'react';
 
 const ContactsManageSearch = () => {
 
+  const inputRef = useRef(null);
+
+  const [searchInputValue, setSearchInputValue] = useState("");
+
   const handleSearchInput = ({ target }) => {
+    setSearchInputValue(target.value);
     console.log("🚀 ~ file: ContactsManageSearch.jsx ~ line 7 ~ handleSearchInput", target.value);
+  };
+
+  const handleResetInput = () => {
+    setSearchInputValue("");
+    inputRef.current.focus();
   };
 
   return (
@@ -13,7 +23,18 @@ const ContactsManageSearch = () => {
         type="text"
         placeholder="Rechercher un contact ou un numéro..."
         onChange={handleSearchInput}
+        value={searchInputValue}
+        ref={inputRef}
       />
+      <button type="button"
+        className="reset-input__button"
+        onClick={handleResetInput}
+      >
+        <div className="reset-input__button__img--container">
+          <img src="/assets/icons/contactsManageIcons/contacts-manage-search-reset.png" className="notHover" />
+          <img src="/assets/icons/contactsManageIcons/contacts-manage-search-reset-hover.png" className="hover" />
+        </div>
+      </button>
     </div>
   );
 };
