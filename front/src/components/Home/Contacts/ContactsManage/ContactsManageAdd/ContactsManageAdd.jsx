bd29c09c4630a794f544439d12cbb1daaf14219a
David Michel <david.michel2809@gmail.com>
2022-07-26 15:48:24 +0200

    feat: adding ContactsManageAdd with base logic #67

--- front/src/components/Home/Contacts/ContactsManage/ContactsManageAdd/ContactsManageAdd.jsx
+++ front/src/components/Home/Contacts/ContactsManage/ContactsManageAdd/ContactsManageAdd.jsx
@@ -1,12 +1,17 @@
 // HOOKS
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 // ICONS
 import addIcon from "/assets/icons/contactsManageIcons/contacts-manage-add-icon.png";
 import addIconHover from "/assets/icons/contactsManageIcons/contacts-manage-add-icon-hover.png";
+import addIconV2 from "/assets/icons/contactsManageIcons/contacts-manage-add-icon-v2.png";
+import closeIcon from "/assets/icons/close-icon.png";
+import closeIconHover from "/assets/icons/close-icon-hover.png";
+
 
 const ContactsManageAdd = () => {
 
   const [isAdding, setIsAdding] = useState(false);
+  const [emailInput, setEmailInput] = useState("");
 
   const handleAdd = (event) => {
     event.preventDefault();
@@ -33,20 +38,27 @@ const ContactsManageAdd = () => {
         <div className="add-contact-menu__container">
           <section className="add-contact-menu">
             <header className="add-contact-menu__header">
-              <img src={addIconHover} className="add-contact-menu__logo" />
+              <img src={addIconV2} className="add-contact-menu__logo" />
               <h2>Windows Live Contacts - Ajouter un contact</h2>
               <button
                 type="button"
-                className="add-contact-menu__close-button"
+                className="close-button"
                 onClick={handleAbort}>
-                annuler
+                <div className="close-button__img--container">
+                  <img src={closeIconHover} className="hover" alt="Icône de fermeture au survol" />
+                  <img src={closeIcon} className="notHover" alt="Icône de fermeture" />
+                </div>
               </button>
             </header>
             <form onSubmit={handleAdd}>
-              <label htmlFor="add-contact-email">Adresse de messagerie instantanée</label>
-              <input type="text" />
+              <label htmlFor="add-contact-email">Adresse de messagerie instantanée :</label>
+              <input type="email" required onChange={({ target }) => setEmailInput(target.value)} />
               <div className="submit-abort">
-                <input type="submit" value="Ajouter un contact" />
+                <input
+                  type="submit"
+                  className={emailInput ? "" : "disabled-email"}
+                  disabled={emailInput ? false : true}
+                  value="Ajouter un contact" />
                 <button type="button" onClick={handleAbort}>Annuler</button>
               </div>
             </form>
