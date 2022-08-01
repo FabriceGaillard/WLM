a2eabd97c3b11f17ec2b12f848013b550e9fa269
David Michel <david.michel2809@gmail.com>
2022-08-01 13:18:43 +0200

    creating settings context and making provider in Home #73

--- front/src/components/Home/Home.jsx
+++ front/src/components/Home/Home.jsx
@@ -1,18 +1,37 @@
+// HOOKS
+import { useContext, useEffect, useState } from "react";
 // COMPONENTS
 import Contacts from "./Contacts/Contacts";
 import User from "./User/User";
-
 import HeaderWithLogo from "../HeaderWithLogo";
+// CONTEXT 
+import settingsContext from '../../contexts/settingsContext';
+// HELPERS
+import { getLocalStorageSettings, addStorageSettings } from '../../helpers/handleStorage';
+// DATA
+import defaultStorageSettings from '../../data/home/defaultStorageSettings';
 
 const Home = () => {
+
+  const [settings, setSettings] = useState(null);
+
+  useEffect(() => {
+    const localStorageSettings = getLocalStorageSettings();
+
+    addStorageSettings(localStorageSettings || defaultStorageSettings);
+    setSettings(localStorageSettings || defaultStorageSettings);
+  }, []);
+
   return (
-    <div className="home__container">
-      <div className="home">
-        <HeaderWithLogo />
-        <User />
-        <Contacts />
+    <settingsContext.Provider value={{ settings, setSettings }}>
+      <div className="home__container">
+        <div className="home">
+          <HeaderWithLogo />
+          <User />
+          <Contacts />
+        </div>
       </div>
-    </div>
+    </settingsContext.Provider>
   );
 };
 
