7d8588d4560bbae34248a84ddafe1815c807d48e
David Michel <david.michel2809@gmail.com>
2022-08-02 09:15:00 +0200

    feat: adding showDetails helper logic #73

--- front/src/helpers/home/contacts/show/showDetails.js
+++ front/src/helpers/home/contacts/show/showDetails.js
@@ -1,5 +1,21 @@
-const showDetails = () => {
-  console.log("showDetails");
+// HELPERS
+import { addStorageSettings } from '../../../handleStorage';
+// DATA
+import checkedStatus from '../../../../data/home/contacts/checkedStatus';
+
+const showDetails = (target, settings, setSettings) => {
+  let { checked } = target.dataset;
+
+  const updateSettings = {
+    ...settings,
+    sortShowOptions: {
+      ...settings.sortShowOptions,
+      showDetails: checkedStatus[checked]
+    }
+  };
+
+  setSettings(updateSettings);
+  addStorageSettings(updateSettings);
 };
 
 export default showDetails;
