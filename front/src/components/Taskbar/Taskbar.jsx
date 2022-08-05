3de5bac79f05ad7b2a32c6648f787f669ceb3723
David Michel <david.michel2809@gmail.com>
2022-08-05 15:20:53 +0200

    style: adding taskbar and taskbar buttons style and background gradient #80

--- front/src/components/Taskbar/Taskbar.jsx
+++ front/src/components/Taskbar/Taskbar.jsx
@@ -2,6 +2,8 @@
 import { useContext } from 'react';
 // CONTEXT
 import globalContext from '../../contexts/GlobalContext';
+// ICONS
+import contactsManageIcon from "/assets/icons/base-icon-hq.png";
 
 const Taskbar = () => {
 
@@ -10,7 +12,10 @@ const Taskbar = () => {
   return (
     <div className="taskbar">
       <button className="start-button">start</button>
-      <button onClick={() => setShowMenu(prev => !prev)}>MSN Messenger</button>
+      <button className={`menu-button ${showMenu ? "focus" : ""}`} onClick={() => setShowMenu(prev => !prev)}>
+        <img src={contactsManageIcon} />
+        <p>MSN Messenger</p>
+      </button>
     </div>
   );
 };
