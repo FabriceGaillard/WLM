4750b2d3c00cd7262d29197157ee395b466f6d5d
David Michel <david.michel2809@gmail.com>
2022-08-08 17:54:03 +0200

    feat: adding close, extend and reduce svg icons #82

---
+++ front/src/icons/extendWindow.jsx
@@ -0,0 +1,8 @@
+
+const extendWindow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M3 21V3h18v18Zm2-2h14V5H5Zm0 0V5v14Z"></path></svg>
+  );
+};
+
+export default extendWindow;
---
+++ front/src/icons/reduceWindow.jsx
@@ -0,0 +1,8 @@
+
+const reduceWindow = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="0.5em" height="1em" viewBox="0 0 8 10"><path fillRule="evenodd" d="M0 7v2h8V7H0z" fill="currentColor"></path></svg>
+  );
+};
+
+export default reduceWindow;
